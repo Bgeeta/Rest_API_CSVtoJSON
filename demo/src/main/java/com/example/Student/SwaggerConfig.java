aa53aa5249bc2f25336f8409b004582be3afe576
Bgeeta <112501195+Bgeeta@users.noreply.github.com>
2023-09-21 10:59:48 +0530

    Update SwaggerConfig.java

--- demo/src/main/java/com/example/Student/SwaggerConfig.java
+++ demo/src/main/java/com/example/Student/SwaggerConfig.java
@@ -24,26 +24,7 @@ public class SwaggerConfig {
             .build();
     }
     
-    // @Bean
-    // public Docket api() {
-    //     return new Docket(DocumentationType.SWAGGER_2)
-    //             .select()
-    //             .apis(RequestHandlerSelectors.basePackage("com.example.Student"))
-    //             .paths(PathSelectors.any())
-    //             .build().apiInfo(apiInfo());
-    // }
 
-    // private ApiInfo apiInfo() {
-    //     ApiInfo apiInfo = new ApiInfo(
-    //             "StudentSearchApplication",
-    //             "An application to search Student from a Student repository by studentId",
-    //             "StudentSearchApplication v1",
-    //             "Terms of service",
-    //             "hendisantika@gmail.com",
-    //             "License of API",
-    //             "https://swagger.io/docs/");
-    //     return apiInfo;
-    // }
 
 
 }
